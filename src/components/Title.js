eab4d718bb025de1fae517568dd9e0cbecf20703
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-02-02 13:54:59 +0100

    Add files

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+
+
+const Title = props => <h1>{props.title}</h1>
+
+/// to jak wiadomo musi być przeze mnie  powtórzone więc teraz bez podpowiedzi się nie obejdzie
+
+
+///export default Title; - to w ogóle jest ppotrzebna???
+
+
