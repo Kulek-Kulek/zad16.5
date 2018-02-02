2f60238428f43ac18284663cc6c6595790b1e36e
Kulek-Kulek <33715566+Kulek-Kulek@users.noreply.github.com>
2018-02-02 13:58:55 +0100

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,9 +2,9 @@
 
 const Title = props => <h1>{props.title}</h1>
 
-/// to jak wiadomo musi być przeze mnie  powtórzone więc teraz bez podpowiedzi się nie obejdzie
+// to jak wiadomo musi być przeze mnie  powtórzone więc teraz bez podpowiedzi się nie obejdzie
 
 
-///export default Title; - to w ogóle jest ppotrzebna???
+//export default Title; - to w ogóle jest ppotrzebna???
 
 
